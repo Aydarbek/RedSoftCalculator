04c011fc7e246a59b08b1e9edc8d5ba0423f1ffa
Aydar Akhmetshin <a88236@gmail.com>
2019-01-25 10:08:25 +0300

    Test calculator is done

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+
+namespace RedSoftQuiz
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            string inStr = Console.ReadLine();
+            DataTable dt = new DataTable();
+            var result = dt.Compute(inStr, "");
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
